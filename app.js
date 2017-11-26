f9baf87d236760e7587bcd358cc29a71c550dd63
Łukasz Szydlik <szydlik.lukasz@gmail.com>
2017-11-26 19:19:20 +0000

    update db url

--- app.js
+++ app.js
@@ -17,10 +17,12 @@ var campgroundRoutes    = require("./routes/campgrounds");
 var indexRoutes         = require("./routes/index");
 
 
+console.log(process.env.DATABASEURL);
 
-//mongoose.connect("mongodb://localhost/yelp_camp_v5", {useMongoClient: true});
-mongoose.connect("mongodb://lukasz:cat@ds121716.mlab.com:21716/yelpcamp", {useMongoClient: true});
-mongodb://lukasz:cat@ds121716.mlab.com:21716/yelpcamp
+mongoose.connect("process.env.DATABASEURL", {useMongoClient: true});
+// mongoose.connect("mongodb://lukasz:cat@ds121716.mlab.com:21716/yelpcamp", {useMongoClient: true});
+process.env.databaseURL
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
